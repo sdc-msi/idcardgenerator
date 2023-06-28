5f11bfaa7e06df67088c9b915e79948bb32a2c9d
Mohd Noor Aman <nooraman5718@gmail.com>
2023-06-28 12:30:04 +0530

    Regex for every columns headers
    I have added regex entry for every possible names a person can define for entries

--- id_generator.py
+++ id_generator.py
@@ -37,15 +37,40 @@ ID_data=pd.read_excel(excel_sheet)
 last_val = ID_data.index[-1]
 print(current_dir)
 
-name_pattern = r'[Nn]ame'
-father_name_pattern = r'[Ff]ather*'
+NAME = r'[Nn]ame'
+FATHER_NAME = r'[Ff]ather*'
+DOB = r'[Dd]ate [Oo]f [Bb]irth|DOB|dob|DoB'
+RANK = r'[Cc]et [Rr]ank|CET*|[Rr]ank*]'
+NUMBER = r'[Nn]umber|[Nn]o.|[Nn]o|[Pp]hone [Nn]umber|[Pp]hone|[Mm]obile*|[Mm]ob'
+COURSE = r'[Cc]ourse|[Cc]ourse [Nn]ame'
+BATCH = r'[Bb]atch|[Bb]atch [Yy]ear'
+PHOTO = r'[Pp]hoto'
+SIGNATURE = r'[Ss]ignature'
+ADDRESS = r'[Pp]ermanent [Aa]ddress|[Aa]ddress|[Aa]ddress [Ll]ine'
+
 
 for columns_header in ID_data.columns:
-    if re.match(name_pattern, col):
+    if re.match(NAME, columns_header):
         name = columns_header
-    if re.match(father_name_pattern, col):
+    if re.match(FATHER_NAME, columns_header):
         father_name = columns_header
-
+    if re.match(DOB, columns_header):
+        dob = columns_header
+    if re.match(RANK, columns_header):
+        rank = columns_header
+    if re.match(NUMBER, columns_header):
+        number = columns_header
+    if re.match(COURSE, columns_header):
+        course = columns_header
+    #THIS IS FOR BATCH BUT NEEDS TO BE CHANGED
+    if re.match(COURSE, columns_header):
+        batch = columns_header
+    if re.match(PHOTO, columns_header):
+        photo = columns_header
+    if re.match(SIGNATURE, columns_header):
+        signature = columns_header
+    if re.match(ADDRESS, columns_header):
+        address = columns_header
 
 for index, row in ID_data.head(last_val).iterrows()    :
 
@@ -53,14 +78,14 @@ for index, row in ID_data.head(last_val).iterrows()    :
     'logo': f'{current_dir}/logo.webp',
     'name': ID_data.iloc[index][name],
     'father_name': ID_data.iloc[index][father_name],
-    'dob': ID_data.iloc[index]['Date of birth'],
-    'cet_rank': ID_data.iloc[index]['CET RANK'],
-    'phone': ID_data.iloc[index]['Phone Number'],
-    'course': ID_data.iloc[index]['Course '],
-    'batch': ID_data.iloc[index]['Course '],
-    'photo': f'{photos_folder[0]}'+ID_data.iloc[index]['Photo'],
-    'signature': f'{sign_folder[0]}'+ID_data.iloc[index]['Signature'],
-    'personal_address': ID_data.iloc[index]['Permanent Address']
+    'dob': ID_data.iloc[index][dob],
+    'cet_rank': ID_data.iloc[index][rank],
+    'phone': ID_data.iloc[index][number],
+    'course': ID_data.iloc[index][course],
+    'batch': ID_data.iloc[index][batch],
+    'photo': f'{photos_folder[0]}'+ID_data.iloc[index][photo],
+    'signature': f'{sign_folder[0]}'+ID_data.iloc[index][signature],
+    'personal_address': ID_data.iloc[index][address]
 }
 
     template = Template(template_html)
