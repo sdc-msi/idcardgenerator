717af866b3296a9458609d05b177d49292b4677b
Mohd Noor Aman <nooraman5718@gmail.com>
2023-06-28 14:39:55 +0530

    Optimized the REGEX data fields

--- id_generator.py
+++ id_generator.py
@@ -37,56 +37,42 @@ ID_data=pd.read_excel(excel_sheet)
 last_val = ID_data.index[-1]
 print(current_dir)
 
-NAME = r'[Nn]ame'
-FATHER_NAME = r'[Ff]ather*'
-DOB = r'[Dd]ate [Oo]f [Bb]irth|DOB|dob|DoB'
-RANK = r'[Cc]et [Rr]ank|CET*|[Rr]ank*]'
-NUMBER = r'[Nn]umber|[Nn]o.|[Nn]o|[Pp]hone [Nn]umber|[Pp]hone|[Mm]obile*|[Mm]ob'
-COURSE = r'[Cc]ourse|[Cc]ourse [Nn]ame'
-BATCH = r'[Bb]atch|[Bb]atch [Yy]ear'
-PHOTO = r'[Pp]hoto'
-SIGNATURE = r'[Ss]ignature'
-ADDRESS = r'[Pp]ermanent [Aa]ddress|[Aa]ddress|[Aa]ddress [Ll]ine'
-
-
-for columns_header in ID_data.columns:
-    if re.match(NAME, columns_header):
-        name = columns_header
-    if re.match(FATHER_NAME, columns_header):
-        father_name = columns_header
-    if re.match(DOB, columns_header):
-        dob = columns_header
-    if re.match(RANK, columns_header):
-        rank = columns_header
-    if re.match(NUMBER, columns_header):
-        number = columns_header
-    if re.match(COURSE, columns_header):
-        course = columns_header
-    #THIS IS FOR BATCH BUT NEEDS TO BE CHANGED
-    if re.match(COURSE, columns_header):
-        batch = columns_header
-    if re.match(PHOTO, columns_header):
-        photo = columns_header
-    if re.match(SIGNATURE, columns_header):
-        signature = columns_header
-    if re.match(ADDRESS, columns_header):
-        address = columns_header
-
-for index, row in ID_data.head(last_val).iterrows()    :
-
-    data = {
-    'logo': f'{current_dir}/logo.webp',
-    'name': ID_data.iloc[index][name],
-    'father_name': ID_data.iloc[index][father_name],
-    'dob': ID_data.iloc[index][dob],
-    'cet_rank': ID_data.iloc[index][rank],
-    'phone': ID_data.iloc[index][number],
-    'course': ID_data.iloc[index][course],
-    'batch': ID_data.iloc[index][batch],
-    'photo': f'{photos_folder[0]}'+ID_data.iloc[index][photo],
-    'signature': f'{sign_folder[0]}'+ID_data.iloc[index][signature],
-    'personal_address': ID_data.iloc[index][address]
+column_patterns = {
+    'name': r'[Nn]ame',
+    'father_name': r'[Ff]ather.',
+    'dob': r'[Dd]ate [Oo]f [Bb]irth|DOB|dob|DoB',
+    'rank': r'[Cc]et [Rr]ank|CET*|[Rr]ank*]',
+    'number': r'[Nn]umber|[Nn]o\.?|[Pp]hone [Nn]umber|[Pp]hone|[Mm]obile*|[Mm]ob',
+    'course': r'[Cc]ourse|[Cc]ourse [Nn]ame',
+    'batch': r'[Bb]atch|[Bb]atch [Yy]ear',
+    'photo': r'[Pp]hoto',
+    'signature': r'[Ss]ignature',
+    'address': r'[Pp]ermanent [Aa]ddress|[Aa]ddress|[Aa]ddress [Ll]ine'
 }
+
+for key, pattern in column_patterns.items():
+        for column_header in ID_data.columns:
+            if re.match(pattern, column_header):
+                column_patterns[key] = column_header
+                break
+
+
+for index, row in ID_data.head(last_val).iterrows():
+    for key, pattern in column_patterns.items():
+        data = {
+        'logo': f'{current_dir}/logo.webp',
+        'name': ID_data.iloc[index][column_patterns['name']],
+        'father_name': ID_data.iloc[index][column_patterns['father_name']],
+        'dob': ID_data.iloc[index][column_patterns['dob']],
+        'cet_rank': ID_data.iloc[index][column_patterns['rank']],
+        'phone': ID_data.iloc[index][column_patterns['number']],
+        'course': ID_data.iloc[index][column_patterns['course']],
+        'batch': ID_data.iloc[index][column_patterns['batch']],
+        'photo': f'{photos_folder[0]}'+ID_data.iloc[index][column_patterns['photo']],
+        'signature': f'{sign_folder[0]}'+ID_data.iloc[index][column_patterns['signature']],
+        'personal_address': ID_data.iloc[index][column_patterns['address']]
+    }
+
 
     template = Template(template_html)
     output_html = template.render(data)
